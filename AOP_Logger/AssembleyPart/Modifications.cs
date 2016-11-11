b5cde4f4fd68517ae6ea1951ecc67da9a6f8171d
Nikita Chernyshov <NikkOrange96@gmail.com>
2016-11-11 20:54:06 +0300

    Start Modify
    Start Modify

---
+++ AOP_Logger/AssembleyPart/Modifications.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyPart
+{
+    public class Modifications
+    {
+        private string fileName;
+
+        public Modifications(string fileName)
+        {
+            this.fileName = fileName;
+        }
+    }
+}
---
+++ AOP_Logger/AssembleyPart/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AssemblyPart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+                if (File.Exists(args[0]))
+                {
+                    Modifications assemblyModifier = new Modifications(args[0]);
+                    Console.WriteLine("Выполнение изменений", args[0]);
+                }
+                else
+                    Console.WriteLine("Файл не найден", args[0]);
+        }
+    }
+}
